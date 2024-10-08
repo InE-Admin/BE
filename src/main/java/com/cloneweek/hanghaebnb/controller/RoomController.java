02e811da96b137c391847b5bafc4fbe1e8f4fdf1
ine <ine.gachon@gmail.com>
2024-10-08 10:23:09 +0900

    Revert "Second Clone Commit"
    This reverts commit 90708e9bab498b4a3b3088de534c94d0e86ef1e5.

--- src/main/java/com/cloneweek/hanghaebnb/controller/RoomController.java
+++ src/main/java/com/cloneweek/hanghaebnb/controller/RoomController.java
@@ -14,8 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile; 
-
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
